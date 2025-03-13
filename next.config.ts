cca44c112a0d73d49f4fb93f5fc2fc931c6e7901
olonibua tolulope <olonibua0001@gmail.com>
2025-03-13 12:10:01 +0000

    new

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  // Your existing config
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ["cloud.appwrite.io"],
+  },
 };
 
 export default nextConfig;
